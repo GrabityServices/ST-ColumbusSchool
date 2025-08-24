236b40928f7425c8860d1eb6d66b4bbf9da948c7
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-08-24 18:32:09 +0530

    Updated

--- public/script/script.js
+++ public/script/script.js
@@ -1,23 +1,21 @@
-const hamburger = document.querySelector( '.hamburger' );
-const links = document.querySelector( '.links' );
-const admission = document.querySelector( '.admission' );
+const link = document.querySelector( ".links" );
+const menuIcon = document.getElementById( "menu-bar" );
+const admission = document.querySelector( ".admission" );
 
-hamburger.addEventListener( 'click', () => {
-    const isActive = hamburger.classList.toggle( 'active' );
-    if ( isActive ) {
-        links.style.display = 'flex';
-        admission.style.display = 'flex';
+menuIcon.addEventListener( "click", () => {
+    if ( menuIcon.classList.value == "fa-solid fa-bars" ) {
+        menuIcon.classList.add( "fa-xmark" );
+        menuIcon.classList.remove( "fa-bars" );
+        link.style.display = "flex";
+        admission.style.display = "flex";
         requestAnimationFrame( () => {
-            links.classList.add( 'active' );
-            admission.classList.add( 'active' );
+            link.classList.add( "active" );
+            admission.classList.add( "active" );
         } );
     } else {
-        links.classList.remove( 'active' );
-        admission.classList.remove( 'active' );
-        setTimeout( () => {
-            links.style.display = 'none';
-            admission.style.display = 'none';
-        }, 300 );
+        menuIcon.classList.remove( "fa-xmark" );
+        menuIcon.classList.add( "fa-bars" );
+        link.classList.remove( "active" );
+        admission.classList.remove( "active" );
     }
-    hamburger.setAttribute( 'aria-expanded', isActive );
 } );
