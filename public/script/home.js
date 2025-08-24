0defe2f9aca361ad67d985e76ae31b3589bd5cde
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-08-24 22:23:03 +0530

    Updated

--- public/script/home.js
+++ public/script/home.js
@@ -1,23 +1,23 @@
-const slides = document.querySelectorAll( '.slide' );
-let currentIndex = 0;
+// const slides = document.querySelectorAll( '.slide' );
+// let currentIndex = 0;
 
-function goToSlide ( index ) {
-    slides.forEach( ( slide, i ) => {
-        // Reset animation
-        slide.style.animation = "none";
-        slide.offsetHeight; // force reflow
-        slide.style.animation = "";
-        slide.style.animation = "slide 12s infinite";
-        slide.style.animationDelay = ( ( i - index ) * 3 ) + "s"; // shift slides
-    } );
-}
+// function goToSlide ( index ) {
+//     slides.forEach( ( slide, i ) => {
+//         // Reset animation
+//         slide.style.animation = "none";
+//         slide.offsetHeight; // force reflow
+//         slide.style.animation = "";
+//         slide.style.animation = "slide 12s infinite";
+//         slide.style.animationDelay = ( ( i - index ) * 3 ) + "s"; // shift slides
+//     } );
+// }
 
-document.getElementById( 'prev' ).addEventListener( 'click', () => {
-    currentIndex = ( currentIndex - 1 + slides.length ) % slides.length;
-    goToSlide( currentIndex );
-} );
+// document.getElementById( 'prev' ).addEventListener( 'click', () => {
+//     currentIndex = ( currentIndex - 1 + slides.length ) % slides.length;
+//     goToSlide( currentIndex );
+// } );
 
-document.getElementById( 'next' ).addEventListener( 'click', () => {
-    currentIndex = ( currentIndex + 1 ) % slides.length;
-    goToSlide( currentIndex );
-} );
+// document.getElementById( 'next' ).addEventListener( 'click', () => {
+//     currentIndex = ( currentIndex + 1 ) % slides.length;
+//     goToSlide( currentIndex );
+// } );
