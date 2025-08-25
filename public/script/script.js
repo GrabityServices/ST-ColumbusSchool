8b1ec7db3006222d20efc96835b055deb7c51ad8
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-08-25 10:16:05 +0530

    Updated

--- public/script/script.js
+++ public/script/script.js
@@ -3,11 +3,13 @@ const menuIcon = document.getElementById( "menu-bar" );
 const admission = document.querySelector( ".admission" );
 
 menuIcon.addEventListener( "click", () => {
-    if ( menuIcon.classList.value == "fa-solid fa-bars" ) {
+    if ( menuIcon.classList.contains( "fa-bars" ) ) {
         menuIcon.classList.add( "fa-xmark" );
         menuIcon.classList.remove( "fa-bars" );
+
         link.style.display = "flex";
         admission.style.display = "flex";
+
         requestAnimationFrame( () => {
             link.classList.add( "active" );
             admission.classList.add( "active" );
@@ -15,6 +17,7 @@ menuIcon.addEventListener( "click", () => {
     } else {
         menuIcon.classList.remove( "fa-xmark" );
         menuIcon.classList.add( "fa-bars" );
+
         link.classList.remove( "active" );
         admission.classList.remove( "active" );
     }
