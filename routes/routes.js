f3fb376068431391549e7e996f32b9c673471bf1
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-08-26 08:40:14 +0530

    Update admins api

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const stcolumbus=express.Router()
-const handelGet=require('../handler/get.js')
-const getAdmin=require('../handler/handelAdmin.js')
-stcolumbus.route('/').get(handelGet)
-stcolumbus.route('/admin').get(getAdmin)
-module.exports=stcolumbus
+const express = require("express");
+const stcolumbus = express.Router();
+const multer =require('multer')
+const avatarupload=require('../utils/updateAdmin.js')
+
+const update =multer({dest:'assets/adminAvt/'})
+
+const handelGet = require("../handler/get.js");
+const {
+  getAdmin,
+  setAdminForm,
+  setAdminFormDet
+} = require("../handler/handelAdmin.js");
+stcolumbus.route("/").get(handelGet);
+stcolumbus.route("/admin").get(getAdmin);
+stcolumbus
+  .route("/adminUpdate/:id")
+  .get((req, res) => res.render("updateAdmin.ejs",{id:req.params.id}))
+  .post(avatarupload.single('img'),setAdminForm)
+
+  stcolumbus.route('/adminUpdate/details/:id')
+  .get((req, res) => res.render("updateAdminDet.ejs",{id:req.params.id}))
+  .post(setAdminFormDet)
+module.exports = stcolumbus;
