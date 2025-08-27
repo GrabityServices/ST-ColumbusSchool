8190c3c06a82d453e005e2ba644e04eb3e55ea78
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-08-27 22:06:53 +0530

    Model for gallery schema

---
+++ models/gallery.js
@@ -0,0 +1,33 @@
+const moment = require("moment");
+const mongoose = require("mongoose");
+
+const gallerySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minlength: [3, "Admin Name must be at least 3 characters"],
+      maxlength: [60, "Admin Name must be at most 30 characters"],
+    },
+    desc: {
+      type: String,
+      minlength: [3, "Position must be at least 3 characters"],
+      maxlength: [500, "Position must be at most 30 characters"],
+    },
+    img: {
+      type: String,
+      default: "/adminAvt/defaultAvt.png",
+    },
+    EventDate: {
+      // type:Date,
+      type: String,
+      default: () => moment().format("YYYY-MM-DD"),
+    },
+  },
+  { timestamps: true }
+);
+
+const Gallery = mongoose.model("gallery", gallerySchema);
+
+
+module.exports = Gallery;
