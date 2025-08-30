a453307a36afe0cae4073ba04b3b4759e4e684cc
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-08-30 14:39:09 +0530

    Admin accounts routes and routes update

---
+++ routes/adminRoutes.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const adminroute = express.Router();
+const adminManagment = express.Router();
+const adminAccountAvt = require("../utils/updateAdminAccountImg.js");
+// ======================accpunt Handlers==============
+const {
+  handleStAdmin,
+  hadnleLoginForm,
+  handleForgotForm,
+  hadnleSignupForm,
+} = require("../handler/stcolumbusadmin.js");
+//==============UserAdmin Updates Handler=============================
+const {
+  handleUpdateAdminImg,
+  handleUpdateAdminDet,
+} = require("../handler/stcolumbusadmin.js");
+//==================admin work=================================
+adminroute.route("").get(handleStAdmin);
+adminroute
+  .route("/update/img/:id")
+  .get((req, res) =>
+    res.render("updateAdminAccountImg.ejs", { id: req.params.id })
+  )
+  .post(adminAccountAvt.single("img"), handleUpdateAdminImg);
+
+adminroute
+  .route("/update/det/:id")
+  .get((req, res) =>
+    res.render("updateAdminAccountDet.ejs", { id: req.params.id })
+  )
+  .post(handleUpdateAdminDet);
+//=======================admin login logout section=========================================
+adminManagment
+  .route("/login")
+  .get((req, res) => {
+    if (req.cookies.stadminis) {
+      return res.redirect("/stcolumbus/jaj/ekdara/admin");
+    }
+    res.render("loginForm.ejs");
+  })
+  .post(hadnleLoginForm);
+
+adminManagment
+  .route("/signup")
+  .get((req, res) => res.render("signupForm.ejs"))
+  .post(hadnleSignupForm);
+
+adminManagment.route("/forgot/userid").get((req, res) => res.send("By Id"));
+adminManagment.route("/forgot/email").get((req, res) => res.send("By email"));
+adminManagment
+  .route("/forgot/pass")
+  .get((req, res) => res.render("forgotAdminPass.ejs", { user: false }))
+  .post(handleForgotForm);
+
+adminManagment.route("/change/pass").get((req, res) => {
+  res.render("forgotAdminPass.ejs", { user: true });
+});
+
+module.exports = { adminroute, adminManagment };
