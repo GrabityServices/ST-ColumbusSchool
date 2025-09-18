94841fdbef0f3336a1bc7e233b7732c8c96e2d80
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-09-18 16:55:46 +0530

    Updated

---
+++ public/script/admin.js
@@ -0,0 +1,24 @@
+const checkbox = document.getElementById( "byDate" );
+const byDateGallery = document.querySelector( ".byDate" );
+const byTitleGallery = document.querySelector( ".byTitle" );
+
+checkbox.addEventListener( "change", () => {
+    if ( checkbox.checked ) {
+        byDateGallery.style.display = "block";
+        byTitleGallery.style.display = "none";
+    } else {
+        byDateGallery.style.display = "none";
+        byTitleGallery.style.display = "block";
+    }
+} );
+
+// Initialize the display based on the checkbox state
+if ( checkbox.checked ) {
+    byDateGallery.style.display = "block";
+    byTitleGallery.style.display = "none";
+} else {
+    byDateGallery.style.display = "none";
+    byTitleGallery.style.display = "block";
+}
+
+const toggleGallery = document.getElementById( "toggle-gallery" );
